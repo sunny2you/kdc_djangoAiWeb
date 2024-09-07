869fcbbe085c99cfe64c1376f9e4948b078e2b22
sunnny2you <ally1008@naver.com>
2024-09-07 21:29:01 +0900

    guessnumbers generator test code

--- lotto/admin.py
+++ lotto/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import GuessNumbers #.는 현재파일과 동일한 계층
 # Register your models here.
+
+admin.site.register(GuessNumbers)
--- lotto/models.py
+++ lotto/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
+import random
 
 # Create your models here.
+
+class GuessNumbers(models.Model):
+    name=models.CharField(max_length=24) #로또 번호 리스트의 이름
+    text=models.CharField(max_length=255) #로또 번호 리스트에 대한 설명
+    lottos=models.CharField(max_length=255,default='[1,2,3,4,5,6]') #로또 번호들이 담길 str
+    num_lotto=models.IntegerField(default=5) #6개 번호 set의 갯수
+    update_date = models.DateTimeField()
+
+    def generate(self): #로또 번호 자동 생성 
+        self.lottos=""
+        origin=list(range(1,46)) #1~45의 숫자 리스트
+        #6개 뽑아 내어 sorting
+        for _ in range(0,self.num_lotto):
+            random.shuffle(origin)
+            guess=origin[:6]
+            guess.sort()
+            self.lottos+=str(guess)+'`n'
+        self.update_date=timezone.now()
+        self.save()
+
+    def __str__(self):
+        return "pk{}:{}-{}".format(self.pk,self.name,self.text) 
